602e7bfefc9e99270abc5734717b57a015e39d94
Vick Kumar <vick.kumar44@gmail.com>
2017-09-11 14:38:33 -0400

    created login page using axios

--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,9 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.get('/status', (req,res) => {
+app.post('/register', (req,res) => {
   res.send({
-    message: 'Hello World'
+    message: `Hello ${req.body.email}! Your user was registered!`
   })
 });
 
